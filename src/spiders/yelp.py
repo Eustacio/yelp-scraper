5b916cf770069125b629e2d4a5a540205144b708
Eustácio <wallison.gth@gmail.com>
2018-02-03 16:01:44 -0200

    Added a subclass of scrapy.Spider destined to crawl and scrap items from the Yelp website

---
+++ src/spiders/yelp.py
@@ -0,0 +1,8 @@
+from scrapy import Spider
+
+
+class YelpSpider(Spider):
+    """ Spider subclass destined to crawl and scrap items from the Yelp website """
+
+    def parse(self, response):
+        pass
