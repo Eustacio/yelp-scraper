312b51f36afa3f5725c76f86626bb73d89659269
Eustácio <wallison.gth@gmail.com>
2018-02-03 17:10:00 -0200

    overridden the Spider#__init__ method to define the required and optional command-line arguments, and added docstring for the YelpSpider#parse method

--- src/spiders/yelp.py
+++ src/spiders/yelp.py
@@ -1,4 +1,7 @@
 from scrapy import Spider
+from scrapy.http import TextResponse, Request
+
+from ..yelp_service import YelpService
 
 
 class YelpSpider(Spider):
@@ -10,5 +13,31 @@ class YelpSpider(Spider):
     # The domains that this spider is allowed to crawl
     allowed_domains = ['yelp.com']
 
-    def parse(self, response):
+    def __init__(self, find=None, near=None, max_results=3, **kwargs) -> None:
+        """
+        This method will take any spider arguments and copy them to the spider as attributes.
+        Spider arguments are passed through the crawl command using the -a option. For Example:
+        `scrapy crawl yelp -a find=Restaurants -a near=...`
+
+        Args:
+            :param find: what to search in the Yelp website (required)
+            :param near: an location for the search (required)
+            :param max_results: the max of results (default is 3)
+        """
+        super(YelpSpider, self).__init__(**kwargs)
+        self.find = find
+        self.near = near
+        self.max_results = max_results
+
+    def parse(self, response: TextResponse) -> [Request, YelpService]:
+        """
+        This is the default callback used by Scrapy to process downloaded responses, when their
+        requests don’t specify a callback.
+
+        The parse method is in charge of processing the response and returning scraped data
+        and/or more URLs to follow.
+
+        Args:
+            :param response: the response to parse
+        """
         pass
