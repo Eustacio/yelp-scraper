f145003d191a6824e1f850e056e596d4b29d2f09
Eustácio <wallison.gth@gmail.com>
2018-02-03 16:10:35 -0200

    Added the required  attribute "name" and the optional "allowed_domains" attribute to the YelpSpider class

--- src/spiders/yelp.py
+++ src/spiders/yelp.py
@@ -4,5 +4,11 @@ from scrapy import Spider
 class YelpSpider(Spider):
     """ Spider subclass destined to crawl and scrap items from the Yelp website """
 
+    # The name for this spider. This attribute is required by the Scrapy
+    name = 'yelp'
+
+    # The domains that this spider is allowed to crawl
+    allowed_domains = ['yelp.com']
+
     def parse(self, response):
         pass
